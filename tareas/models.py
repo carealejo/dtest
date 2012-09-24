a60d534b0561170f3f80b228372c8fac87d87010
Gustavo A. Gómez Farhat <gustavo.gomez.farhat@gmail.com>
2012-09-24 14:16:40 -0500

    Mejorando la presentación del formulario

--- tareas/models.py
+++ tareas/models.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*- 
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib import admin
@@ -10,8 +12,8 @@ Estado_Tareas = (
 
 class Tarea(models.Model):
     nombre = models.CharField(max_length=200)
-    fechaini = models.DateTimeField('Fecha Inicio')
-    fechafin = models.DateTimeField('Fecha Terminacion')
+    fechaini = models.DateTimeField('Fecha de inicio')
+    fechafin = models.DateTimeField('Fecha de finalización')
     descripcion = models.CharField(max_length=500)
     estado = models.CharField(max_length=4,choices=Estado_Tareas)
     usuario = models.ForeignKey(User)
