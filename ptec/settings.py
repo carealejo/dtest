2b4c8f397b098da791b1a4305b543c7f6df1f9ac
Alejandro Cardona Recalde <carealejo@gmail.com>
2012-09-25 00:00:31 -0500

    Edicion plantilla

--- ptec/settings.py
+++ ptec/settings.py
@@ -67,8 +67,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-#    '/home/alejandro/work/PS/PruTecnica/ptec/tareas/static',
-    '/home/farhat/work/nuevosmedios/dtest/tareas/static',
+    '/home/alejandro/work/PS/PruTecnica/ptec/tareas/static',
+#    '/home/farhat/work/nuevosmedios/dtest/tareas/static',
 )
 
 # List of finder classes that know how to find static files in
